479e546bf3f80147ee71ee156ad1d44c46f4b521
JericSheila <jrhod_baby@yahoo.com>
2017-02-06 10:51:34 +0800

    Initial Files

---
+++ assets/js/script.js
@@ -0,0 +1,193 @@
+$('#wpimg01').waypoint(
+  function(direction) {
+    if(direction=='down'){
+      $("#img01").addClass("show");
+    }
+    else {
+      $("#img01").removeClass("show");
+    }
+  }
+);
+
+$('#wpimg02').waypoint(
+  function(direction) {
+    if(direction=='down'){
+      $("#img01").removeClass("show");
+      $("#img02").addClass("show");
+    }
+    else {
+      $("#img02").removeClass("show");
+      $("#img01").addClass("show");
+    }
+  }
+);
+
+$('#wpimg03').waypoint(
+  function(direction) {
+    if(direction=='down'){
+      $("#img02").removeClass("show");
+      $("#img03").addClass("show");
+    }
+    else {
+      $("#img03").removeClass("show");
+      $("#img02").addClass("show");
+    }
+  }
+);
+
+$('#wpimg04').waypoint(
+  function(direction) {
+    if(direction=='down'){
+      $("#img03").removeClass("show");
+      $("#img04").addClass("show");
+    }
+    else {
+      $("#img04").removeClass("show");
+      $("#img03").addClass("show");
+    }
+  }
+);
+
+$('#wpimg05').waypoint(
+  function(direction) {
+    if(direction=='down'){
+      $("#img04").removeClass("show");
+      $("#img05").addClass("show");
+    }
+    else {
+      $("#img05").removeClass("show");
+      $("#img04").addClass("show");
+    }
+  }
+);
+
+$('#wpimg06').waypoint(
+  function(direction) {
+    if(direction=='down'){
+      $("#img05").removeClass("show");
+      $("#img06").addClass("show");
+    }
+    else {
+      $("#img06").removeClass("show");
+      $("#img05").addClass("show");
+    }
+  }
+);
+
+$('#wpimg07').waypoint(
+  function(direction) {
+    if(direction=='down'){
+      $("#img06").removeClass("show");
+      $("#img07").addClass("show");
+    }
+    else {
+      $("#img07").removeClass("show");
+      $("#img06").addClass("show");
+    }
+  }
+);
+
+$('#wpimg08').waypoint(
+  function(direction) {
+    if(direction=='down'){
+      $("#img07").removeClass("show");
+      $("#img08").addClass("show");
+    }
+    else {
+      $("#img08").removeClass("show");
+      $("#img07").addClass("show");
+    }
+  }
+);
+
+$('#wpimg09').waypoint(
+  function(direction) {
+    if(direction=='down'){
+      $("#img08").removeClass("show");
+      $("#img09").addClass("show");
+    }
+    else {
+      $("#img09").removeClass("show");
+      $("#img08").addClass("show");
+    }
+  }
+);
+
+$('#wpimg10').waypoint(
+  function(direction) {
+    if(direction=='down'){
+      $("#img09").removeClass("show");
+      $("#img10").addClass("show");
+    }
+    else {
+      $("#img10").removeClass("show");
+      $("#img09").addClass("show");
+    }
+  }
+);
+
+$('#wpimg11').waypoint(
+  function(direction) {
+    if(direction=='down'){
+      $("#img10").removeClass("show");
+      $("#img11").addClass("show");
+    }
+    else {
+      $("#img11").removeClass("show");
+      $("#img10").addClass("show");
+    }
+  }
+);
+
+$('#wpimg12').waypoint(
+  function(direction) {
+    if(direction=='down'){
+      $("#img11").removeClass("show");
+      $("#img12").addClass("show");
+    }
+    else {
+      $("#img12").removeClass("show");
+      $("#img11").addClass("show");
+    }
+  }
+);
+
+
+$('#wpimg13').waypoint(
+  function(direction) {
+    if(direction=='down'){
+      $("#img12").removeClass("show");
+      $("#img13").addClass("show");
+    }
+    else {
+      $("#img13").removeClass("show");
+      $("#img12").addClass("show");
+    }
+  }
+);
+
+$('#wpimg14').waypoint(
+  function(direction) {
+    if(direction=='down'){
+      $("#img13").removeClass("show");
+      $("#img14").addClass("show");
+    }
+    else {
+      $("#img14").removeClass("show");
+      $("#img13").addClass("show");
+    }
+  }
+);
+
+$('#wpimg15').waypoint(
+  function(direction) {
+    if(direction=='down'){
+      $("#img14").removeClass("show");
+      $("#img15").addClass("show");
+    }
+    else {
+      $("#img15").removeClass("show");
+      $("#img14").addClass("show");
+    }
+  }
+);
